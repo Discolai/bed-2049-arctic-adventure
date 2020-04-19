4f86445e9450983efe46952e70cda0edee9ada93
Nikolai Norum Hansen <nikolainh@hotmail.com>
2020-04-19 18:26:04 +0200

    Added description to husky sledding

--- src/components/activities/info/sledding.jsx
+++ src/components/activities/info/sledding.jsx
@@ -13,18 +13,23 @@ const Sledding = (props) => {
       title="Enjoy your ride across the beatiful winter landscape"
       included="Transportation, dog sledding, lunch, and thermal suits"
       duration="Sledding: 60 minutes, Dog yard: 30 minutes"
-      departure="17:00-13:00 the next day"
+      departure="09:00:00-10:00"
       pricing="Adults: 1399 NOK, Children: 699 NOK"
       bring="Gloves/mittens and a hat"
     >
-      <p className="activity-info">
-        Please
+      <p>
+        Relax in your sled while one of our experienced mushers drive you
+        across the beatiful winter landscape surrounding Tromsø. This is the ideal
+        trip for taking gorgeous pictures, bringing your family or just relax comfortably.
       </p>
-      <p className="activity-info">
-        fill
+      <p>
+        We provide you with thermal suits and boots before departure so you keep
+        nice and warm. You are then assigned a guide who will take you for a 60 minute
+        drive. Guest over 16 years old will be offered a chance to drive the sled.
       </p>
-      <p className="activity-info">
-        me
+      <p>
+        Upon return to our center, we will bring you to our dog yard where you can
+        pet and snuggle with our alaskan huskies to your hearts desire.
       </p>
     </ActivityInfo>
   )
