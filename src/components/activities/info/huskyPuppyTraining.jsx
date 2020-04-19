4fcaffba285ffecc6c70f8e4c2355b7d9b6a3447
Nikolai Norum Hansen <nikolainh@hotmail.com>
2020-04-19 20:57:36 +0200

    Added description for puppy training

--- src/components/activities/info/huskyPuppyTraining.jsx
+++ src/components/activities/info/huskyPuppyTraining.jsx
@@ -10,21 +10,24 @@ const HuskyPuppyTraining = (props) => {
     <ActivityInfo
       background={puppy_training}
       caption="husky puppy training"
-      title="Visit our dog yard with over 300 huskies and train some puppies"
+      title="Visit center with over 300 huskies and train some puppies"
       included="Transportation, rain coats, rubber boots and lunch"
       duration="5 hours"
       departure="09:00-14:00"
       pricing="Adults: 999 NOK, Children: 499 NOK"
       bring=""
     >
-      <p className="activity-info">
-        Please
+      <p>
+        We breed our own huskies, so we get multiple litters of puppies a year. Dog sledding
+        requires extensive training, which you can be a part of.
       </p>
-      <p className="activity-info">
-        fill
+      <p>
+        The main training session is a light hike where we teach our puppies to cooperate with
+        each other and humans.
       </p>
-      <p className="activity-info">
-        me
+      <p>
+        Upon return to our center, we will serve dinner made from local produce supplied by Eide
+        Handel. The meals are primarily meat based, but we offer vegetarian options upon request.
       </p>
     </ActivityInfo>
   )
