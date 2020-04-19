b36c762c03e5c87829d69f4eee23c757e82b10f7
Nikolai Norum Hansen <nikolainh@hotmail.com>
2020-04-19 19:20:53 +0200

    Added description to kayaking

--- src/components/activities/info/kayaking.jsx
+++ src/components/activities/info/kayaking.jsx
@@ -12,19 +12,24 @@ const Kayaking = (props) => {
       caption="Sea kayaking"
       title="Explore the fjords surrounding Tromsø under the midnight sun"
       included="Transportation, dinner, kayaking equipment"
-      duration="5 hours"
-      departure="21:00-02:00"
+      duration="6 hours"
+      departure="20:00-02:00"
       pricing="Adults: 1399 NOK, Children: 699 NOK"
       bring="Warm clothes, sunglasses, sneakers, a dry change of clothes"
     >
-      <p className="activity-info">
-        Please
+      <p>
+        Experience the unique midnight sun and the beatiful fjords surrounding
+        Tromsø to the sound of nothing, but the slight splashes of the ocean and
+        the circling sea birds.
       </p>
-      <p className="activity-info">
-        fill
+      <p>
+        We serve dinner at our centre before we set out from our center.
+        The meals are primarily meat based, but we offer vegetarian
+        options upon request.
       </p>
-      <p className="activity-info">
-        me
+      <p>
+        We supply you with double kayaks, which are ideal for beginners. We offer single
+        kayaks for the more experienced guests upon request.
       </p>
     </ActivityInfo>
   )
